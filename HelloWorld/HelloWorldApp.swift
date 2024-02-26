e2e9a7db4b5932da9136f0bec32c890ae9a1539c
Srujan Dharma <Srujan.23bcs10107@ms.sst.scaler.com>
2024-02-26 17:28:23 +0530

    added all files

--- HelloWorld/HelloWorldApp.swift
+++ HelloWorld/HelloWorldApp.swift
@@ -2,19 +2,21 @@
 //  HelloWorldApp.swift
 //  HelloWorld
 //
-//  Created by Sai Venkatesh Alampally on 26/02/24.
+//  Created by Srujan Dharma on 26/02/24.
 //
 
 import SwiftUI
 
 @main
-struct HelloWorldApp: App {
+struct MyApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+               .background(.black.opacity(0.8))
         }
 
-        ImmersiveSpace(id: "ImmersiveSpace") {
+
+        ImmersiveSpace(id: "Immersive") {
             ImmersiveView()
         }
     }
